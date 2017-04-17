0e33680c13058e17759a71c70c95c0496999bb11
electricSemyon <electricSemyon@mail.ru>
2017-04-17 18:02:49 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+ 
+module.exports = {
+  entry: './src/app.js',
+  output: { path: __dirname + '/dist', filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.less$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "less-loader" // compiles Less to CSS
+        }]
+      }
+    ]
+  }
+};
