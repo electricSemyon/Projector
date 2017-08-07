51f366d2efc4d38198e872f3023e541730a587d5
electricSemyon <electricSemyon@gmail.com>
2017-08-07 20:24:52 +0300

    Добавил тесты для BoardItem

--- src/test/utils/validate.test.js
+++
@@ -1,8 +0,0 @@
-import {expect} from 'chai';
-import validate from '../utils/validate';
-
-describe('validate', () => {
-  it('password should be at least 6 characters', () => {
-    expect(validate('passwprd', 'wrong')).to.be(false);
-  })
-})
---
+++ test/utils/board-item.test.js
@@ -0,0 +1,12 @@
+import 'jsdom-global/register';
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+import BoardItem from '../../src/components/boards-list/board-item.jsx';
+
+describe("BoardItem", function() {
+  it("should render three headers", function() {
+    const wrapper = mount(<BoardItem name={'Sample board'}/>);
+    expect(wrapper.find('h3')).to.have.length(3);
+  });
+ });
